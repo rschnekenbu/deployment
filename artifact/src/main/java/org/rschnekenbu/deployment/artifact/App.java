62e16f2cfeefe259574196dfd34d775c0d88931e
Remi Schnekenburger <rschnekenburger@eclipsesource.com>
2019-02-26 17:18:49 +0100

    Update the main #2

--- artifact/src/main/java/org/rschnekenbu/deployment/artifact/App.java
+++ artifact/src/main/java/org/rschnekenbu/deployment/artifact/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Simple update of master branch." );
+        System.out.println( "Hello World! Simple update #2 of master branch." );
     }
 }
